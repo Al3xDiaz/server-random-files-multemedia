0094295456a013134dcc4c4c36e15f3123c2686c
al3xdiaz <alexleonel96@hotmail.com>
2023-05-11 23:10:10 -0600

    [fix] next random video

--- components/videos/index.tsx
+++ components/videos/index.tsx
@@ -3,14 +3,19 @@ import React,{useCallback,useState} from "react";
 
 
 export const Videos = (props:{files:IFile[]}) => {
+    const videoRef = React.useRef<HTMLVideoElement>(null);
     const [currentVideo,setCurrentVideo] = useState(props.files[0]);
     const nextRandomVideo = useCallback(()=>{
+        console.log("nextRandomVideo");        
         setCurrentVideo(props.files[Math.floor(Math.random()*props.files.length)]);
-    },[props.files]);
+    },[,setCurrentVideo])
+    React.useEffect(()=>{
+        videoRef.current?.load();
+    },[currentVideo])
 
     return (
     <div>
-        <video controls>
+        <video controls ref={videoRef} autoPlay>
             <source src={currentVideo.path} type={/mp4/.test(currentVideo.path)?"video/mp4":"video/webm"} />
             Your browser does not support the video tag.
         </video>
